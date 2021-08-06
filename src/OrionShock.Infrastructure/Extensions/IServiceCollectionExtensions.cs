d2bd25b2a02b5054706d2ef7ec9e8a86550fd37b
ivanbiljan <ssbiljan@gmail.com>
2021-08-06 22:48:45 +0200

    Add IServiceCollection extensions

---
+++ src/OrionShock.Infrastructure/Extensions/IServiceCollectionExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrionShock.Infrastructure.Extensions
+{
+    public static class IServiceCollectionExtensions
+    {
+        public static void AddOrionShockDbContext(this IServiceCollection serviceCollection, string connectionString)
+        {
+            if (serviceCollection is null)
+            {
+                throw new ArgumentNullException(nameof(serviceCollection));
+            }
+
+            if (connectionString is null)
+            {
+                throw new ArgumentNullException(nameof(connectionString));
+            }
+
+            serviceCollection.AddDbContext<OrionShockDbContext>(opts => opts.UseNpgsql(connectionString));
+        }
+    }
+}
