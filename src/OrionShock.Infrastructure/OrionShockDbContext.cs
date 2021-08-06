2d35067606d3db87adbded02345b4469cf846f8a
ivanbiljan <ssbiljan@gmail.com>
2021-08-06 22:52:45 +0200

    Define Warps DbSet

--- src/OrionShock.Infrastructure/OrionShockDbContext.cs
+++ src/OrionShock.Infrastructure/OrionShockDbContext.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OrionShock.Infrastructure.Models;
 using System;
 
 namespace OrionShock.Infrastructure
 {
     public sealed class OrionShockDbContext : DbContext
     {
+        public OrionShockDbContext(DbContextOptions<OrionShockDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Warp> Warps { get; set; }
     }
 }
