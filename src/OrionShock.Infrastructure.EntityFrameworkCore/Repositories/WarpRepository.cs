871e46c2309f01d3bb6b10604585e3d27b413965
ivanbiljan <ssbiljan@gmail.com>
2021-08-11 19:44:58 +0200

    Add EfRepositoryBase

---
+++ src/OrionShock.Infrastructure.EntityFrameworkCore/Repositories/WarpRepository.cs
@@ -0,0 +1,22 @@
+﻿using OrionShock.Infrastructure.EntityFrameworkCore.Persistence;
+using OrionShock.Infrastructure.Shared.Models;
+using OrionShock.Infrastructure.Shared.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrionShock.Infrastructure.EntityFrameworkCore.Repositories
+{
+    internal sealed class WarpRepository : EfRepositoryBase<Warp>, IWarpRepository
+    {
+        public WarpRepository(OrionShockDbContext context) : base(context)
+        {
+        }
+
+        public Warp GetWarpByName(string name) => Context.Warps!.FirstOrDefault(w => w.Name == name);
+
+        public Warp GetWarpByPosition(int tileX, int tileY) => Context.Warps!.FirstOrDefault(w => w.TileX == tileX && w.TileY == tileY);
+    }
+}
